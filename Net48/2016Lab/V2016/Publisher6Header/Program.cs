8b04645cd993425b2d895bba55cfbc3d80e321c7
Honda <hondachen@hotmail.com>
2023-02-24 10:35:46 +0800

    Add Part 9. Headers

---
+++ Net48/2016Lab/V2016/Publisher6Header/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// add
+using RabbitMQ.Client;
+//using RabbitMQ.Client.MessagePatterns;
+using RabbitMQ.Client.Events;
+
+namespace Publisher6Header
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SetUpHeadersExchange();
+        }
+        private static void SetUpHeadersExchange()
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+
+            connectionFactory.Port = 5672;
+            connectionFactory.HostName = "localhost";
+            connectionFactory.UserName = "accountant";
+            connectionFactory.Password = "accountant";
+            connectionFactory.VirtualHost = "accounting";
+
+            IConnection connection = connectionFactory.CreateConnection();
+            IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("company.exchange.headers", ExchangeType.Headers, true, false, null);
+            channel.QueueDeclare("company.queue.headers", true, false, false, null);
+            Dictionary<string, object> headerOptionsWithAll = new Dictionary<string, object>();
+            headerOptionsWithAll.Add("x-match", "all");
+            headerOptionsWithAll.Add("category", "animal");
+            headerOptionsWithAll.Add("type", "mammal");
+
+            channel.QueueBind("company.queue.headers", "company.exchange.headers", "", headerOptionsWithAll);
+
+            Dictionary<string, object> headerOptionsWithAny = new Dictionary<string, object>();
+            headerOptionsWithAny.Add("x-match", "any");
+            headerOptionsWithAny.Add("category", "plant");
+            headerOptionsWithAny.Add("type", "tree");
+
+            channel.QueueBind("company.queue.headers", "company.exchange.headers", "", headerOptionsWithAny);
+
+            IBasicProperties properties = channel.CreateBasicProperties();
+            Dictionary<string, object> messageHeaders = new Dictionary<string, object>();
+            messageHeaders.Add("category", "animal");
+            messageHeaders.Add("type", "insect");
+            properties.Headers = messageHeaders;
+            PublicationAddress address = new PublicationAddress(ExchangeType.Headers, "company.exchange.headers", "");
+            channel.BasicPublish(address, properties, Encoding.UTF8.GetBytes("Hello from the world of insects"));
+
+            properties = channel.CreateBasicProperties();
+            messageHeaders = new Dictionary<string, object>();
+            messageHeaders.Add("category", "animal");
+            messageHeaders.Add("type", "mammal");
+            messageHeaders.Add("mood", "awesome");
+            properties.Headers = messageHeaders;
+            channel.BasicPublish(address, properties, Encoding.UTF8.GetBytes("Hello from the world of awesome mammals"));
+
+            properties = channel.CreateBasicProperties();
+            messageHeaders = new Dictionary<string, object>();
+            messageHeaders.Add("category", "animal");
+            messageHeaders.Add("type", "mammal");
+            properties.Headers = messageHeaders;
+            channel.BasicPublish(address, properties, Encoding.UTF8.GetBytes("Hello from the world of mammals"));
+
+            properties = channel.CreateBasicProperties();
+            messageHeaders = new Dictionary<string, object>();
+            messageHeaders.Add("category", "animal");
+            properties.Headers = messageHeaders;
+            channel.BasicPublish(address, properties, Encoding.UTF8.GetBytes("Hello from the world of animals"));
+
+            properties = channel.CreateBasicProperties();
+            messageHeaders = new Dictionary<string, object>();
+            messageHeaders.Add("category", "fungi");
+            messageHeaders.Add("type", "champignon");
+            properties.Headers = messageHeaders;
+            channel.BasicPublish(address, properties, Encoding.UTF8.GetBytes("Hello from the world of fungi"));
+
+            properties = channel.CreateBasicProperties();
+            messageHeaders = new Dictionary<string, object>();
+            messageHeaders.Add("category", "plant");
+            properties.Headers = messageHeaders;
+            channel.BasicPublish(address, properties, Encoding.UTF8.GetBytes("Hello from the world of plants"));
+
+            properties = channel.CreateBasicProperties();
+            messageHeaders = new Dictionary<string, object>();
+            messageHeaders.Add("category", "plant");
+            messageHeaders.Add("type", "tree");
+            properties.Headers = messageHeaders;
+            channel.BasicPublish(address, properties, Encoding.UTF8.GetBytes("Hello from the world of trees"));
+
+            properties = channel.CreateBasicProperties();
+            messageHeaders = new Dictionary<string, object>();
+            messageHeaders.Add("mood", "sad");
+            messageHeaders.Add("type", "tree");
+            properties.Headers = messageHeaders;
+            channel.BasicPublish(address, properties, Encoding.UTF8.GetBytes("Hello from the world of sad trees"));
+
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
---
+++ Net48/2016Lab/V2016/Receiver6Header/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// add
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receiver6Header
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReceiveMessagesWithEvents();
+        }
+        private static void ReceiveMessagesWithEvents()
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+
+            connectionFactory.Port = 5672;
+            connectionFactory.HostName = "localhost";
+            connectionFactory.UserName = "accountant";
+            connectionFactory.Password = "accountant";
+            connectionFactory.VirtualHost = "accounting";
+
+            IConnection connection = connectionFactory.CreateConnection();
+            IModel channel = connection.CreateModel();
+            channel.BasicQos(0, 1, false);
+            EventingBasicConsumer eventingBasicConsumer = new EventingBasicConsumer(channel);
+
+            eventingBasicConsumer.Received += (sender, basicDeliveryEventArgs) =>
+            {
+                IBasicProperties basicProperties = basicDeliveryEventArgs.BasicProperties;
+                Console.WriteLine("Message received by the event based consumer. Check the debug window for details.");
+                Debug.WriteLine(string.Concat("Message received from the exchange ", basicDeliveryEventArgs.Exchange));
+                Debug.WriteLine(string.Concat("Message: ", Encoding.UTF8.GetString(basicDeliveryEventArgs.Body.ToArray())));
+                StringBuilder headersBuilder = new StringBuilder();
+                headersBuilder.Append("Headers: ").Append(Environment.NewLine);
+                foreach (var kvp in basicProperties.Headers)
+                {
+                    headersBuilder.Append(kvp.Key).Append(": ").Append(Encoding.UTF8.GetString(kvp.Value as byte[])).Append(Environment.NewLine);
+                }
+                Debug.WriteLine(headersBuilder.ToString());
+                channel.BasicAck(basicDeliveryEventArgs.DeliveryTag, false);
+            };
+
+            channel.BasicConsume("company.queue.headers", false, eventingBasicConsumer);
+        }
+    }
+}
