9de4fbaf3176241ca3aaafcb2010323afb2824ad
Honda <hondachen@hotmail.com>
2023-03-12 19:45:36 +0800

    Add rpc publisher4 and consumer4.

---
+++ Net48/2016Lab/V2016/VOriginal/Publisher4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// add
+using RabbitMQ.Client;
+using Service1;
+
+namespace Publisher4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AmqpMessagingService messagingService = new AmqpMessagingService();
+            IConnection connection = messagingService.GetRabbitMqConnection();
+            IModel model = connection.CreateModel();
+            //messagingService.SetUpQueueForRpcDemo(model);
+        }
+        private static void RunRpcMessageDemo(IModel model, AmqpMessagingService messagingService)
+        {
+            Console.WriteLine("Enter your message and press Enter. Quit with 'q'.");
+            while (true)
+            {
+                string message = Console.ReadLine();
+                if (message.ToLower() == "q") break;
+                String response = messagingService.SendRpcMessageToQueue(message, model, TimeSpan.FromMinutes(1));
+                Console.WriteLine("Response: {0}", response);
+            }
+        }
+    }
+}
--- Net48/2016Lab/V2016/VOriginal/Service1/AmqpMessagingService.cs
+++ Net48/2016Lab/V2016/VOriginal/Service1/AmqpMessagingService.cs
@@ -3,7 +3,11 @@ using RabbitMQ.Client.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Runtime.Remoting.Channels;
 using System.Text;
+using System.Threading;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace Service1
@@ -24,6 +28,13 @@ namespace Service1
         private string _publishSubscribeExchangeName = "PublishSubscribeExchange";
         private string _publishSubscribeQueueOne = "PublishSubscribeQueueOne";
         private string _publishSubscribeQueueTwo = "PublishSubscribeQueueTwo";
+
+        // rpc
+        private string _rpcQueueName = "RpcQueue";
+        //private QueueingBasicConsumer _rpcConsumer;
+        private EventingBasicConsumer _rpcConsumer;
+        private string _responseQueue;
+
         public IConnection GetRabbitMqConnection()
         {
             ConnectionFactory connectionFactory = new ConnectionFactory();
@@ -48,6 +59,10 @@ namespace Service1
             model.QueueDeclare(_publishSubscribeQueueTwo, true, false, false, null);
             model.QueueBind(_publishSubscribeQueueOne, _publishSubscribeExchangeName, "");
             model.QueueBind(_publishSubscribeQueueTwo, _publishSubscribeExchangeName, "");
+        }
+        public void SetUpQueueForRpcDemo(IModel model)
+        {
+            model.QueueDeclare(_rpcQueueName, _durable, false, false, null);
         }
 
         public void SendOneWayMessage(string message, IModel model)
@@ -74,6 +89,74 @@ namespace Service1
             byte[] messageBytes = Encoding.UTF8.GetBytes(message);
             model.BasicPublish(_publishSubscribeExchangeName, "", basicProperties, messageBytes);
         }
+
+        // 20230312, 新版原始碼, 大多將 IModel model 命名為 channel.
+        // public string SendRpcMessageToQueue(string message, IModel model, TimeSpan timeout)
+        public string SendRpcMessageToQueue(string message, IModel channel, TimeSpan timeout)
+        {
+            if (string.IsNullOrEmpty(_responseQueue))
+            {
+                _responseQueue = channel.QueueDeclare().QueueName;
+            }
+
+            if (_rpcConsumer == null)
+            {
+                //_rpcConsumer = new QueueingBasicConsumer(model);
+                _rpcConsumer = new EventingBasicConsumer(channel);
+                channel.BasicConsume(_responseQueue, true, _rpcConsumer);
+            }
+
+            string correlationId = Guid.NewGuid().ToString();
+            string responseFromConsumer = null;
+            bool bResponse = false;
+
+            IBasicProperties basicProperties = channel.CreateBasicProperties();
+            basicProperties.ReplyTo = _responseQueue;
+            basicProperties.CorrelationId = correlationId;
+
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish("", _rpcQueueName, basicProperties, messageBytes);
+
+
+            //// 20230308, change to EventingBasicConsumer
+            //DateTime timeoutDate = DateTime.UtcNow + timeout;
+            //while (DateTime.UtcNow <= timeoutDate)
+            //{
+            //    BasicDeliverEventArgs deliveryArguments = (BasicDeliverEventArgs)_rpcConsumer.Queue.Dequeue();
+            //    if (deliveryArguments.BasicProperties != null
+            //    && deliveryArguments.BasicProperties.CorrelationId == correlationId)
+            //    {
+            //        string response = Encoding.UTF8.GetString(deliveryArguments.Body.ToArray());
+            //        return response;
+            //    }
+            //}
+            //throw new TimeoutException("No response before the timeout period.");
+
+            _rpcConsumer.Received += (sender, basicDeliveryEventArgs) => 
+            {
+                IBasicProperties props = basicDeliveryEventArgs.BasicProperties;
+                if (basicDeliveryEventArgs.BasicProperties != null
+                && basicDeliveryEventArgs.BasicProperties.CorrelationId == correlationId)
+                {
+                    string response = Encoding.UTF8.GetString(basicDeliveryEventArgs.Body.ToArray());
+                    responseFromConsumer = response;
+                    bResponse = true;
+                }
+                //bool bMultiple = false;
+                //channel.BasicAck(basicDeliveryEventArgs.DeliveryTag, bMultiple);
+                ////Console.WriteLine("Consumer response: {0}", responseFromConsumer);
+            };
+            Thread.Sleep(timeout);
+            if (bResponse)
+                return responseFromConsumer;
+            else
+            {
+                //bool bAutoAct = false;
+                //channel.BasicConsume(_responseQueue, bAutoAct, _rpcConsumer);
+                throw new TimeoutException("No response before the timeout period.");
+            }
+        }
+
         public void ReceiveOneWayMessages(IModel model)
         {
             // 20230303, Honda, Change QueueingBasicConsumer
@@ -140,6 +223,42 @@ namespace Service1
             model.BasicQos(0, 1, false);
             MyDefaultBasicConsumer consumer = new MyDefaultBasicConsumer(model);
             model.BasicConsume(_publishSubscribeQueueTwo, false, consumer);
+        }
+        public void ReceiveRpcMessage(IModel model)
+        {
+            // 20230312, 舊寫法為 loop, 新寫法改用 EventingBasicConsumer
+            //model.BasicQos(0, 1, false);
+            //QueueingBasicConsumer consumer = new QueueingBasicConsumer(model);
+            //model.BasicConsume(_rpcQueueName, false, consumer);
+
+            //while (true)
+            //{
+            //    BasicDeliverEventArgs deliveryArguments = consumer.Queue.Dequeue() as BasicDeliverEventArgs;
+            //    string message = Encoding.UTF8.GetString(deliveryArguments.Body);
+            //    Console.WriteLine("Message: {0} ; {1}", message, " Enter your response: ");
+            //    string response = Console.ReadLine();
+            //    IBasicProperties replyBasicProperties = model.CreateBasicProperties();
+            //    replyBasicProperties.CorrelationId = deliveryArguments.BasicProperties.CorrelationId;
+            //    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+            //    model.BasicPublish("", deliveryArguments.BasicProperties.ReplyTo, replyBasicProperties, responseBytes);
+            //    model.BasicAck(deliveryArguments.DeliveryTag, false);
+            //}
+            model.BasicQos(0, 1, false);
+            EventingBasicConsumer consumer = new EventingBasicConsumer(model);
+            model.BasicConsume(_rpcQueueName, false, consumer);
+            consumer.Received += (sender, basicDeliveryEventArgs) =>
+            {
+                IBasicProperties props = basicDeliveryEventArgs.BasicProperties;
+                string message = Encoding.UTF8.GetString(basicDeliveryEventArgs.Body.ToArray());
+                Console.WriteLine("Message: {0} ; {1}", message, " Enter your response: ");
+                string response = Console.ReadLine();
+
+                IBasicProperties replyBasicProperties = model.CreateBasicProperties();
+                replyBasicProperties.CorrelationId = props.CorrelationId;
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                model.BasicPublish("", props.ReplyTo, replyBasicProperties, responseBytes);
+                model.BasicAck(basicDeliveryEventArgs.DeliveryTag, false);
+            };
         }
     }
 }
